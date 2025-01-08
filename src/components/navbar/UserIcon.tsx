870031b5f0e04f46f9c8149e7765278dc5cf5f0f
lesterflor <155178241+lesterflor@users.noreply.github.com>
2025-01-08 13:47:19 -0500

    set up Clerk authentication

--- src/components/navbar/UserIcon.tsx
+++ src/components/navbar/UserIcon.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
+import { LuUserRound } from 'react-icons/lu';
 
 export default function UserIcon() {
-	return <div>UserIcon</div>;
+	return <LuUserRound className='w-6 h-6 bg-primary rounded-full text-white' />;
 }

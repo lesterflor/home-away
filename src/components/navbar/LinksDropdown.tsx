d9c3cc4b6725ce3257ea59e2728f62b3866c8f0e
lesterflor <155178241+lesterflor@users.noreply.github.com>
2025-01-08 14:07:09 -0500

    set up authentication

--- src/components/navbar/LinksDropdown.tsx
+++ src/components/navbar/LinksDropdown.tsx
@@ -3,15 +3,16 @@ import {
 	DropdownMenu,
 	DropdownMenuContent,
 	DropdownMenuItem,
-	DropdownMenuTrigger
-	//DropdownMenuSeparator
+	DropdownMenuTrigger,
+	DropdownMenuSeparator
 } from '@/components/ui/dropdown-menu';
 import { LuAlignLeft } from 'react-icons/lu';
 import Link from 'next/link';
 import { Button } from '../ui/button';
 import UserIcon from './UserIcon';
 import { links } from '@/utils/link';
-//import SignOutLink from './SignOutLink';
+import SignOutLink from './SignOutLink';
+import { SignedOut, SignedIn, SignInButton, SignUpButton } from '@clerk/nextjs';
 
 export default function LinksDropdown() {
 	return (
@@ -28,17 +29,36 @@ export default function LinksDropdown() {
 				className='w-52'
 				align='start'
 				sideOffset={10}>
-				{links.map((link) => {
-					return (
-						<DropdownMenuItem key={link.href}>
-							<Link
-								href={link.href}
-								className='capitalize w-full'>
-								{link.label}
-							</Link>
-						</DropdownMenuItem>
-					);
-				})}
+				<SignedOut>
+					<DropdownMenuItem>
+						<SignInButton mode='modal'>
+							<button className='w-full text-left'>Login</button>
+						</SignInButton>
+					</DropdownMenuItem>
+					<DropdownMenuSeparator />
+					<DropdownMenuItem>
+						<SignUpButton mode='modal'>
+							<button className='w-full text-left'>Sign Up</button>
+						</SignUpButton>
+					</DropdownMenuItem>
+				</SignedOut>
+				<SignedIn>
+					{links.map((link) => {
+						return (
+							<DropdownMenuItem key={link.href}>
+								<Link
+									href={link.href}
+									className='capitalize w-full'>
+									{link.label}
+								</Link>
+							</DropdownMenuItem>
+						);
+					})}
+					<DropdownMenuSeparator />
+					<DropdownMenuItem>
+						<SignOutLink />
+					</DropdownMenuItem>
+				</SignedIn>
 			</DropdownMenuContent>
 		</DropdownMenu>
 	);

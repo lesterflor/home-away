33a55d9cf62dca8668b83351baabbd5d3af7cf56
lesterflor <155178241+lesterflor@users.noreply.github.com>
2025-01-09 15:39:13 -0500

    create auth check for sign in

--- src/components/card/FavoriteToggleButton.tsx
+++ src/components/card/FavoriteToggleButton.tsx
@@ -1,11 +1,19 @@
 import { FaHeart } from 'react-icons/fa';
 import { Button } from '@/components/ui/button';
+import { auth } from '@clerk/nextjs/server';
+import { CardSignInButton } from '../form/Buttons';
 
 export default function FavoriteToggleButton({
 	propertyId
 }: {
 	propertyId: string;
 }) {
+	const { userId } = auth();
+
+	if (!userId) {
+		return <CardSignInButton />;
+	}
+
 	return (
 		<Button
 			size='icon'

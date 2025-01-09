dcae6b0036576efe02a03809d5fa001b4612eb6e
lesterflor <155178241+lesterflor@users.noreply.github.com>
2025-01-09 12:57:34 -0500

    add db record for property

--- src/utils/actions.ts
+++ src/utils/actions.ts
@@ -167,13 +167,23 @@ export const createPropertyAction = async (
 	const user = await getAuthUser();
 	try {
 		const rawData = Object.fromEntries(formData);
+		const file = formData.get('image') as File;
 
 		const validatedFields = validateWithZodSchema(propertySchema, rawData);
 
-		//redirect('/');
+		const validatedFile = validateWithZodSchema(imageSchema, { image: file });
 
-		return { message: 'Property successfully created' };
+		const fullPath = await uploadImage(validatedFile.image);
+
+		await db.property.create({
+			data: {
+				...validatedFields,
+				image: fullPath,
+				profileId: user.id
+			}
+		});
 	} catch (error) {
 		return renderError(error);
 	}
+	redirect('/');
 };

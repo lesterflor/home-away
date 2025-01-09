a3de45a35d4d166f7aa5b59fbd23e48cb3a73a55
lesterflor <155178241+lesterflor@users.noreply.github.com>
2025-01-09 14:24:16 -0500

    property card progress

--- src/components/home/PropertiesList.tsx
+++ src/components/home/PropertiesList.tsx
@@ -7,7 +7,7 @@ export default function PropertiesList({
 	properties: PropertyCardProps[];
 }) {
 	return (
-		<section>
+		<section className='mt-4 gap-8 grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
 			{properties.map((item) => {
 				return (
 					<PropertyCard

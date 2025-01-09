c8c0a1bf465f7234f0023d4e2bac44bfc3c84656
lesterflor <155178241+lesterflor@users.noreply.github.com>
2025-01-09 17:58:02 -0500

    property detail components progress

--- src/app/properties/[id]/page.tsx
+++ src/app/properties/[id]/page.tsx
@@ -1,10 +1,14 @@
 import FavoriteToggleButton from '@/components/card/FavoriteToggleButton';
 import PropertyRating from '@/components/card/PropertyRating';
+import Amenities from '@/components/properties/Amenities';
 import BookingCalendar from '@/components/properties/booking/BookingCalendar';
 import BreadCrumbs from '@/components/properties/BreadCrumbs';
+import Description from '@/components/properties/Description';
 import ImageContainer from '@/components/properties/ImageContainer';
 import PropertyDetails from '@/components/properties/PropertyDetails';
 import ShareButton from '@/components/properties/ShareButton';
+import UserInfo from '@/components/properties/UserInfo';
+import { Separator } from '@/components/ui/separator';
 import { fetchPropertyDetails } from '@/utils/actions';
 import { redirect } from 'next/navigation';
 import React from 'react';
@@ -23,6 +27,9 @@ export default async function PropertyDetailsPage({
 	const { baths, bedrooms, beds, guests } = property;
 
 	const details = { baths, bedrooms, beds, guests };
+
+	const firstName = property.profile.firstName;
+	const profileImage = property.profile.profileImage;
 
 	return (
 		<section>
@@ -43,6 +50,7 @@ export default async function PropertyDetailsPage({
 				mainImage={property.image}
 				name={property.name}
 			/>
+
 			<section className='lg:grid lg:grid-cols-12 gap-x-12 mt-12'>
 				<div className='lg:col-span-8'>
 					<div className='flex gap-x-4 items-center'>
@@ -53,7 +61,13 @@ export default async function PropertyDetailsPage({
 						/>
 					</div>
 					<PropertyDetails details={details} />
+					<UserInfo profile={{ firstName, profileImage }} />
+					<Separator className='mt-4' />
+					<Description description={property.description} />
+
+					<Amenities amenities={property.amenities} />
 				</div>
+
 				<div className='lg:col-span-4 flex flex-col items-center'>
 					{/* calendar */}
 					<BookingCalendar />
